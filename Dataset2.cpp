c61e1b497d1c3a68a272e09b0a4f1a77e2a0a198
Rony Lee <rongyil33@gmail.com>
2024-01-22 13:56:44 +0800

    Replace Dataset2

--- Dataset2.cpp
+++ Dataset2.cpp
@@ -7,170 +7,130 @@ using namespace std;
 const double ID1 = 1211306086;
 const double ID2 = 1211307001;
 
-struct Route {
-  char from, to;
-  int distance;
+struct Route
+{
+    char from, to;
+    int distance;
 };
 
-struct Station {
-  char name;
-  int x, y, z;
-  int weight, profit;
-  vector<Route> routes;
+struct Station
+{
+    char name;
+    int x, y, z;
+    int weight, profit;
+    vector<Route> routes;
 };
 
-int calculateDistance(const Station &s1, const Station &s2) {
-  return sqrt(pow(s1.x - s2.x, 2) + pow(s1.y - s2.y, 2) + pow(s1.z - s2.z, 2));
+int calculateDistance(const Station &s1, const Station &s2)
+{
+    return sqrt(pow(s1.x - s2.x, 2) + pow(s1.y - s2.y, 2) + pow(s1.z - s2.z, 2));
 }
 
-void detectSameStationAndRoute(Station &station1, Station &station2,
-                               vector<Station> &stations) { // O(n^3)
-  bool routeExist = false;
-  do {
-    station2 = stations[rand() % 20];
+void generateRoute(int stationIndex1, vector<Station> &stations, int &totalGeneratedRoutes)
+{
+    int stationIndex2 = rand() % 20;
 
-    if (station1.name == station2.name) // detectSameStation
-      continue;
-
-    for (const auto &route : station1.routes) { // detectSameStationAndRoute
-      if (route.to == station2.name)
-        continue;
-      routeExist = true;
+    // prevent station1 pointing to itself
+    while (stationIndex1 == stationIndex2)
+    {
+        stationIndex2 = rand() % 20;
     }
-  } while (routeExist == false);
-}
 
-void updateStation(Station &station, vector<Station> &stations) {
-  for (Station &s : stations) {
-    if (s.name == station.name) {
-      s = station;
-      break;
+    Station station1 = stations[stationIndex1];
+    Station station2 = stations[stationIndex2];
+
+    // prevent duplicated routes
+    bool routeExist = false;
+    for (const auto &route : station1.routes)
+    {
+        if (route.to == station2.name)
+        {
+            routeExist = true;
+            break;
+        }
     }
-  }
-}
 
-void generateRoute(Station &station1,
-                   vector<Station> &stations) { // non-directed graph
-                                                // generate one specific route
-  Station station2 = stations[rand() % 20];
+    if (!routeExist)
+    {
+        Route route;
+        route.from = station1.name;
+        route.to = station2.name;
+        route.distance = calculateDistance(station1, station2);
+        station1.routes.push_back(route);
+        stations[stationIndex1] = station1;
 
-  // CASES
-  // 1. cannot route back to same station
-  // 2. cannot route to same station twice
-  // 3. if A -> B, then B -> A
-
-  // (1. same station)
-  // (2. same route)
-  detectSameStationAndRoute(station1, station2, stations);
-
-  Route route;
-  route.from = station1.name;
-  route.to = station2.name;
-  route.distance = calculateDistance(station1, station2);
-  station1.routes.push_back(route);
-  // replace station1 with its index in stations
-  updateStation(station1, stations);
-
-  // (3. A -> B, B -> A)
-  Route route2;
-  route2.from = station2.name;
-  route2.to = station1.name;
-  route2.distance = calculateDistance(station2, station1);
-  station2.routes.push_back(route2);
-  updateStation(station2, stations);
-}
-
-void generateData(vector<Station> &stations) { // points to stations variable
-  srand(ID1 + ID2);
-  // generate 20 stations without routes
-  for (char stationName = 'A'; stationName <= 'T'; ++stationName) {
-    // A to T is 20 steps
-    Station station;
-    station.name = stationName;
-    station.x = rand() % 80 + 1;
-    station.y = rand() % 80 + 1;
-    station.z = rand() % 80 + 1;
-    station.weight = rand() % 20;
-    station.profit = rand() % 50;
-    stations.push_back(station); // the reason why routes is not saved
-  }
-
-  // generate more routes if any station has less than 3 routes
-  for (int i = 0; i < 20; i++) {
-    Station station1 = stations[i];
-    while (station1.routes.size() < 3) {
-      generateRoute(station1, stations);
+        // reverse
+        Route route2;
+        route2.from = station2.name;
+        route2.to = station1.name;
+        route2.distance = route.distance;
+        station2.routes.push_back(route2);
+        stations[stationIndex2] = station2;
+        totalGeneratedRoutes++;
     }
-  }
 }
 
-// ************
-// NOT DONE YET
-// ************
-vector<vector<int>> generateAdjacencyMatrix(
-    const vector<Station> &stations) { // weighted, and directed graph
-  // Initialize the adjacency matrix with 0s
-  vector<vector<int>> adjacencyMatrix(stations.size(),
-                                      vector<int>(stations.size(), 0));
+void generateData(vector<Station> &stations)
+{
+    srand(ID1 + ID2);
 
-  for (Station station : stations) {
-    // For each route
-    for (const Route &route : station.routes) {
-      // Find the indices of the 'from' and 'to' stations
-      int fromIndex = route.from - 'A';
-      int toIndex = route.to - 'A';
-
-      // Set the corresponding cell in the adjacency matrix to 1
-      adjacencyMatrix[fromIndex][toIndex] = station.weight;
+    // generate 20 stations, from A to T
+    for (char stationName = 'A'; stationName <= 'T'; ++stationName)
+    {
+        Station station;
+        station.name = stationName;
+        station.x = rand() % 80 + 1;
+        station.y = rand() % 80 + 1;
+        station.z = rand() % 80 + 1;
+        station.weight = rand() % 20;
+        station.profit = rand() % 50;
+        stations.push_back(station);
     }
-  }
 
-  // generate route with weight
+    // generate routes
+    int totalGeneratedRoutes = 0;
 
-  return adjacencyMatrix;
-}
-
-void printArray(vector<Station> stations) {
-
-  printf("Printing the station...\n");
-  for (const auto &station : stations) {
-    cout << "Station " << station.name << " " << station.x << " " << station.y
-         << " " << station.z << " " << station.weight << " " << station.profit
-         << endl;
-
-    // printf("Printing the routes...\n");
-    cout << "Printing the routes of Station " << station.name << "..." << endl;
-    for (const Route &route : station.routes) {
-      cout << "Route from Station " << route.from << " to Station " << route.to
-           << ", Distance: " << route.distance << endl;
-      printf("Route printed\n");
+    // target 1: ensure each station connects to at least 3 other stations
+    for (int i = 0; i < 20; i++)
+    {
+        while (stations[i].routes.size() < 3)
+        {
+            generateRoute(i, stations, totalGeneratedRoutes);
+        }
     }
-    printf("\n");
-  }
-}
 
-void printAdjacencyMatrix(vector<vector<int>> adjacencyMatrix) {
-
-  // add column number
-  printf("     ");
-  for (int x = 1; x <= adjacencyMatrix[0].size(); x++) {
-    printf("%2d ", x);
-  }
-  printf(" \n");
-  for (int i = 0; i < adjacencyMatrix.size(); i++) {
-    printf("%2d [ ", i + 1); // add row number
-    for (const auto &cell : adjacencyMatrix[i]) {
-      printf("%2d ", cell);
+    // target 2: ensure there is 54 routes
+    while (totalGeneratedRoutes < 54)
+    {
+        int stationIndex1 = rand() % 20;
+        generateRoute(stationIndex1, stations, totalGeneratedRoutes);
     }
-    printf("]");
-    cout << endl;
-  }
 }
 
-int main() {
-  vector<Station> stations;
-  generateData(stations);
-  vector<vector<int>> adjacencyMatrix = generateAdjacencyMatrix(stations);
-  printArray(stations);
-  printAdjacencyMatrix(adjacencyMatrix);
+void printArray(vector<Station> stations)
+{
+
+    // print stations
+    for (const auto &station : stations)
+    {
+        cout << "Station " << station.name << " " << station.x << " " << station.y
+             << " " << station.z << " " << station.weight << " " << station.profit
+             << endl;
+
+
+        // print routes
+        for (const Route &route : station.routes)
+        {
+            cout << route.from << " -> " << route.to
+                 << ", Distance: " << route.distance << endl;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    vector<Station> stations;
+    generateData(stations);
+    printArray(stations);
 }
