4ec62597d9892963b5565dd69b815b7f419fec47
viv <vivian.wgting11@gmail.com>
2024-01-25 20:11:54 +0800

    Update Station to 2D vector
    - random alphabets
    - station is now 2D
    - route generation updated

--- Dataset2.cpp
+++ Dataset2.cpp
@@ -7,23 +7,44 @@ using namespace std;
 const double ID1 = 1211306086;
 const double ID2 = 1211307001;
 
-struct Route {
+struct Route
+{
   char from, to;
   int distance;
 };
 
-struct Station {
+struct Station
+{
   char name;
   int x, y, z;
   int weight, profit;
   vector<Route> routes;
 };
 
-int calculateDistance(const Station &s1, const Station &s2) {
+int calculateDistance(const Station &s1, const Station &s2)
+{
   return sqrt(pow(s1.x - s2.x, 2) + pow(s1.y - s2.y, 2) + pow(s1.z - s2.z, 2));
 }
 
-void updateRoute(Station &fromStation, Station &toStation) {
+vector<char> generateRandomStationName()
+{
+  vector<char> stationNames;
+  for (char c = 'A'; c <= 'T'; ++c)
+  {
+    stationNames.push_back(c);
+  }
+
+  for (int i = stationNames.size() - 1; i > 0; --i)
+  {
+    int j = rand() % (i + 1);
+    swap(stationNames[i], stationNames[j]);
+  }
+
+  return stationNames;
+}
+
+void updateRoute(Station &fromStation, Station &toStation)
+{
   Route route;
   route.from = fromStation.name;
   route.to = toStation.name;
@@ -31,124 +52,194 @@ void updateRoute(Station &fromStation, Station &toStation) {
   fromStation.routes.push_back(route);
 }
 
-void generateRoute(int stationIndex1, vector<Station> &stations,
-                   int &totalGeneratedRoutes) {
-  int stationIndex2 = rand() % 20;
-
-  // prevent station1 pointing to itself
-  while (stationIndex1 == stationIndex2) {
-    stationIndex2 = rand() % 20;
+bool ifValidRoute(const Station &station1, const Station &station2)
+{
+  if (station1.name == station2.name)
+  {
+    return false;
   }
 
-  Station station1 = stations[stationIndex1];
-  Station station2 = stations[stationIndex2];
-
-  // prevent duplicated routes
-  bool routeExist = false;
-  for (const auto &route : station1.routes) {
-    if (route.to == station2.name) {
-      routeExist = true;
-      break;
+  for (const auto &route : station1.routes)
+  {
+    if (route.to == station2.name)
+    {
+      return false;
     }
   }
 
-  if (!routeExist) {
+  return true;
+}
+
+void generateRoute(int oriRow, int oriCol, vector<vector<Station>> &stations, int &totalGeneratedRoutes)
+{
+  Station station1 = stations[oriRow][oriCol];
+  Station station2;
+  int newRow = oriRow, newCol = oriCol;
+
+  int direction = rand() % 6;
+
+  switch (direction)
+  {
+  case 0: // left
+    newCol = max(0, oriCol - 1);
+    break;
+  case 1: // right
+    newCol = min(3, oriCol + 1);
+    break;
+  case 2: // up
+    newRow = max(0, oriRow - (rand() % 3));
+    break;
+  case 3: // down
+    newRow = min(4, oriRow + (rand() % 3));
+    break;
+  case 4: // right up OR left up
+    if (oriRow == 1 || oriRow == 3)
+    {
+      newCol = min(3, oriCol + 1);
+      newRow = max(0, oriRow - 1);
+    }
+    else
+    {
+      newCol = max(0, oriCol - 1);
+      newRow = max(0, oriRow - 1);
+    }
+    break;
+  case 5: // right down OR left down
+    if (oriRow == 1 || oriRow == 3)
+    {
+      newCol = min(3, oriCol + 1);
+      newRow = min(4, oriRow + 1);
+    }
+    else
+    {
+      newCol = max(0, oriCol - 1);
+      newRow = min(4, oriRow + 1);
+    }
+    break;
+  }
+
+  station2 = stations[newRow][newCol];
+
+  if (ifValidRoute(station1, station2))
+  {
     updateRoute(station1, station2);
-    stations[stationIndex1] = station1;
+    stations[oriRow][oriCol] = station1;
 
     // reverse
     updateRoute(station2, station1);
-    stations[stationIndex2] = station2;
+    stations[newRow][newCol] = station2;
     totalGeneratedRoutes++;
   }
 }
 
-void generateData(vector<Station> &stations) {
+void generateData(vector<vector<Station>> &stations)
+{
   srand(ID1 + ID2);
 
+  // randomise station names
+  vector<char> stationNames = generateRandomStationName();
+
   // generate 20 stations, from A to T
-  for (char stationName = 'A'; stationName <= 'T'; ++stationName) {
-    Station station;
-    station.name = stationName;
-    station.x = rand() % 80 + 1; // [10, 20, 30, 40, 50]
-    station.y = rand() % 80 + 1;
-    station.z = rand() % 80 + 1;
-
-    // Station A {x: 20, y: 40, z: 30}   A -> C
-    // Station B {x: 30, y: 50, z: 80}
-    // Station C {x: 25, y: 40, z: 40}   C -> A
-    // inorder to connect to other station, x or y or z has to be same
-
-    station.weight = rand() % 20;
-    station.profit = rand() % 50;
-    stations.push_back(station);
+  int rows = 5;
+  int cols = 4;
+  int k = 0;
+  for (int i = 0; i < rows; ++i)
+  {
+    for (int j = 0; j < cols; ++j)
+    {
+      stations[i][j].name = stationNames[k++];
+      stations[i][j].x = rand() % 80 + 1;
+      stations[i][j].y = rand() % 80 + 1;
+      stations[i][j].z = rand() % 80 + 1;
+      stations[i][j].weight = rand() % 20;
+      stations[i][j].profit = rand() % 50;
+    }
   }
 
   // generate routes
   int totalGeneratedRoutes = 0;
 
   // target 1: ensure each station connects to at least 3 other stations
-  for (int i = 0; i < 20; i++) {
-    while (stations[i].routes.size() < 3) {
-      generateRoute(i, stations, totalGeneratedRoutes);
+  for (int i = 0; i < rows; ++i)
+  {
+    for (int j = 0; j < cols; ++j)
+    {
+      while (stations[i][j].routes.size() < 3)
+      {
+        generateRoute(i, j, stations, totalGeneratedRoutes);
+      }
     }
   }
 
   // target 2: ensure there is 54 routes
-  while (totalGeneratedRoutes < 54) {
-    int stationIndex1 = rand() % 20;
-    generateRoute(stationIndex1, stations, totalGeneratedRoutes);
+  while (totalGeneratedRoutes < 54)
+  {
+    int i = rand() % rows;
+    int j = rand() % cols;
+    generateRoute(i, j, stations, totalGeneratedRoutes);
   }
 }
 
-vector<vector<int>> generateAdjacencyMatrix(
-    const vector<Station> &stations) { // weighted, and directed graph
+vector<vector<int>> generateAdjacencyMatrix(const vector<vector<Station>> &stations)
+{ // weighted, and directed graph
   // Initialize the adjacency matrix with 0s
-  vector<vector<int>> adjacencyMatrix(stations.size(),
-                                      vector<int>(stations.size(), 0));
+  vector<vector<int>> adjacencyMatrix(20, vector<int>(20, 0));
 
-  for (Station station : stations) {
-    // For each route
-    for (const Route &route : station.routes) {
-      // Find the indices of the 'from' and 'to' stations
-      int fromIndex = route.from - 'A';
-      int toIndex = route.to - 'A';
+  for (const auto &stationRow : stations)
+  {
+    for (const auto &station : stationRow)
+    {
+      for (const Route &route : station.routes)
+      {
+        // Find the indices of the 'from' and 'to' stations
+        int fromIndex = route.from - 'A';
+        int toIndex = route.to - 'A';
 
-      // generate route with weight
-      adjacencyMatrix[fromIndex][toIndex] = station.weight;
-      //}
+        // generate route with weight
+        adjacencyMatrix[fromIndex][toIndex] = station.weight;
+      }
     }
   }
   return adjacencyMatrix;
 }
 
-void printArray(vector<Station> stations) {
+void printArray(vector<vector<Station>> stations)
+{
 
   // print stations
-  for (const auto &station : stations) {
-    cout << "Station " << station.name << " " << station.x << " " << station.y
-         << " " << station.z << " " << station.weight << " " << station.profit
-         << endl;
+  for (const auto &stationRow : stations)
+  {
+    for (const auto &station : stationRow)
+    {
+      cout << "Station " << station.name << " " << station.x << " " << station.y
+           << " " << station.z << " " << station.weight << " " << station.profit
+           << endl;
 
-    // print routes
-    for (const Route &route : station.routes) {
-      cout << route.from << " -> " << route.to
-           << ", Distance: " << route.distance << endl;
+      // print routes
+      for (const Route &route : station.routes)
+      {
+        cout << route.from << " -> " << route.to
+             << ", Distance: " << route.distance << endl;
+      }
+      cout << endl;
     }
-    printf("\n");
   }
 }
 
-void printAdjacencyMatrix(vector<vector<int>> adjacencyMatrix) {
+void printAdjacencyMatrix(vector<vector<int>> adjacencyMatrix)
+{
   //  add column number
   printf("     ");
-  for (int x = 1; x <= adjacencyMatrix[0].size(); x++) {
+  for (int x = 1; x <= adjacencyMatrix[0].size(); x++)
+  {
     printf("%2d ", x);
   }
   printf(" \n");
-  for (int i = 0; i < adjacencyMatrix.size(); i++) {
+  for (int i = 0; i < adjacencyMatrix.size(); i++)
+  {
     printf("%2d [ ", i + 1); // add row number
-    for (const auto &cell : adjacencyMatrix[i]) {
+    for (const auto &cell : adjacencyMatrix[i])
+    {
       printf("%2d ", cell);
     }
     printf("]");
@@ -156,8 +247,9 @@ void printAdjacencyMatrix(vector<vector<int>> adjacencyMatrix) {
   }
 }
 
-int main() {
-  vector<Station> stations;
+int main()
+{
+  vector<vector<Station>> stations(5, vector<Station>(4));
   generateData(stations);
   vector<vector<int>> adjacencyMatrix = generateAdjacencyMatrix(stations);
   printArray(stations);
