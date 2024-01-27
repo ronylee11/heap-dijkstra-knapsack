b570ad067f91f47dea64e0ada5da28874e99ebc7
viv <vivian.wgting11@gmail.com>
2024-01-27 16:29:53 +0800

    update dijikstra & add paths

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -9,57 +9,66 @@ using namespace std;
 
 const int INF = INT_MAX;
 
-int minDistance(const vector<int> &dist, const vector<bool> &sptSet)
-{
-    int min = INF;
-    int u;
-
-    for (int v = 0; v < 20; v++)
-    {
-        if (!sptSet[v] && dist[v] <= min)
-        {
-            min = dist[v];
-            u = v;
-        }
-    }
-
-    return u;
-}
-
 // shortest paths
 void dijkstra(const vector<vector<int>> &adjacencyMatrix, int startStation)
 {
     int totalStations = adjacencyMatrix.size();
 
-    // store the shortest distance from A to all other stations
-    // set the distance of all stations to INF
+    // store shortest distance from A to all other stations
+    // set distance of all stations to INF
     vector<int> dist(totalStations, INF);
 
-    // shortest paths tree
+    // set the distance of src station as 0
+    dist[startStation] = 0;
+
+    // shortest paths tree, store visited stations
     // set all stations unvisited
     vector<bool> spTree(totalStations, false);
 
-    // set the distance of start station as 0
-    dist[startStation] = 0;
+    // store paths
+    vector<vector<int>> paths(totalStations);
 
     // find the shortest paths for all stations
     for (int i = 0; i < totalStations - 1; i++)
     {
-        int u = minDistance(dist, spTree);
-        spTree[u] = true; // set station as visited
+        // pick an unvisited min dist
+        int u = -1;
 
         for (int v = 0; v < totalStations; v++)
+            if (!spTree[v] && (u == -1 || dist[v] <= dist[u]))
+                u = v;    
+
+
+        spTree[u] = true; // include u in sptTree, i.e. set station as visited
+
+        // update dist value of all adjacent stations of u
+        for (int v = 0; v < totalStations; v++)
         {
-            if (!spTree[v] && 
-                adjacencyMatrix[u][v] &&
-                dist[u] != INF &&
-                dist[u] + adjacencyMatrix[u][v] < dist[v])
-                dist[v] = dist[u] + adjacencyMatrix[u][v];
+            if (!spTree[v] &&                              // v not visited
+                adjacencyMatrix[u][v] &&                   // edge uv exist
+                dist[u] + adjacencyMatrix[u][v] < dist[v]) // sum of dist[u] + weight uv < dist[v]
+                {
+                    dist[v] = dist[u] + adjacencyMatrix[u][v];
+                    paths[v] = paths[u];
+                    paths[v].push_back(v);
+                }
         }
     }
 
-    // print
-    cout << "Stations | Distance from A" << endl;
-    for (int i = 0; i < 20; i++)
-        cout << "  " << static_cast<char>('A' + i) << "\t\t" << dist[i] << endl;
+    // print distance & paths from A
+    cout << endl;
+    cout << "Stations | Distance from A | Path from A" << endl;
+    for (int i = 0; i < totalStations; i++) {
+        cout << "  " << static_cast<char>('A' + i) << "\t |\t" << dist[i];
+        cout << "\t   |  A";
+        if (i != 0)
+            cout << " -> ";
+        for (int j = 0; j < paths[i].size(); j++)
+        {
+            if (j != 0)
+                cout << " -> ";
+            cout << static_cast<char>('A' + paths[i][j]);
+        }
+        cout << endl;
+    }
 }
