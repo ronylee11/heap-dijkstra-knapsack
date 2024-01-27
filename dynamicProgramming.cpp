8ba355cd0df6df16a76b191f6a902c4a90e197c3
Rony Lee <rongyil33@gmail.com>
2024-01-27 16:22:27 +0800

    Update 0/1 fractional knapsack

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -1,48 +1,88 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
 #include "structs.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-// Function to perform 0/1 Knapsack
-void knapsack(const vector<Station> &stations, int capacity) {
-  int n = stations.size();
-  vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+int knapsack(int maxWeight, const vector<Station> &stations) {
+  int numOfItems = stations.size();
+  int dpTable[numOfItems + 1][maxWeight + 1];
 
-  // Fill the dp table
-  for (int i = 1; i <= n; ++i) {
-    for (int w = 0; w <= capacity; ++w) {
-      if (stations[i - 1].weight <= w) {
-        dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - stations[i - 1].weight] +
-                                         stations[i - 1].profit);
-      } else {
-        dp[i][w] = dp[i - 1][w];
-      }
+  // dynamic programming table
+  for (int i = 0; i <= numOfItems; i++) {
+    for (int w = 0; w <= maxWeight; w++) {
+      if (i == 0 || w == 0)
+        dpTable[i][w] = 0;
+      else if (stations[i - 1].weight <= w)
+        dpTable[i][w] = max(stations[i - 1].profit +
+                                dpTable[i - 1][w - stations[i - 1].weight],
+                            dpTable[i - 1][w]);
+      else
+        dpTable[i][w] = dpTable[i - 1][w];
     }
   }
 
-  // Find selected items
-  int w = capacity;
-  vector<char> selectedStations;
+  // display table
+  for (int i = 0; i <= numOfItems; i++) { // n is 10 items
+    cout << "item = " << i << "\n";
+    for (int j = 0; j <= maxWeight; j++) { // W is  max weight
+      cout << dpTable[i][j] << " ";
+    }
+    cout << "\n\n";
+  }
 
-  for (int i = n; i > 0 && w > 0;) {
-    if (dp[i][w] != dp[i - 1][w]) {
-      selectedStations.push_back(stations[i - 1].name);
-      w -= stations[i - 1].weight;
-      --i;
+  // find selected items
+  int result = dpTable[numOfItems][maxWeight];
+  int weight = maxWeight;
+  vector<int> selectedStations;
+  int totalWeight = 0;
+
+  for (int i = numOfItems; i > 0 && result > 0; i--) {
+    if (result == dpTable[i - 1][weight]) {
+      continue;
     } else {
-      --i;
+      // if the result is not from the top, then the item is included
+      selectedStations.insert(selectedStations.begin(), i - 1);
+
+      // since this weight is included its profit is deducted   
+      result = result - stations[i - 1].profit;
+      weight = weight - stations[i - 1].weight;
     }
   }
 
-  // Print the selected stations
-  cout << "Selected Stations for maximum profit within knapsack capacity: ";
-  for (int i = selectedStations.size() - 1; i >= 0; --i) {
-    cout << selectedStations[i] << " ";
+  // display all stations
+  cout << "All Stations: \n";
+  cout << "-------------------------------------------------\n| \t\tAll Stations\t\t\t|" << endl;
+  cout << "-------------------------------------------------";
+  cout << "\n|Station name   |weight\t\t|profit\t\t|\n-------------------------------------------------" << endl;
+  for (int i = 0; i <= stations.size() - 1; i++) {
+    cout << "|" <<  stations[i].name << "\t\t|" << stations[i].weight << "\t\t|" << stations[i].profit << "\t\t|" << endl;
   }
-  cout << endl;
+  cout << "-------------------------------------------------\n" << endl;
 
-  // Print the maximum profit
-  cout << "Maximum Profit: " << dp[n][capacity] << endl;
+
+
+  // display selected stations
+  cout << "Selected Stations for maximum profit within knapsack capacity: \n";
+  cout << "-------------------------------------------------\n| \t\tChosen Stations\t\t\t|" << endl;
+  cout << "-------------------------------------------------";
+  cout << "\n|Station name   |weight\t\t|profit\t\t|\n-------------------------------------------------" << endl;
+  for (int i = 0; i <= selectedStations.size() - 1; i++) {
+    cout << "|" << stations[selectedStations[i]].name << "\t\t|" << stations[selectedStations[i]].weight << "\t\t|" << stations[selectedStations[i]].profit << "\t\t|" << endl;
+  }
+  cout << "-------------------------------------------------" << endl;
+
+  // calculate total weight
+  for (int i = 0; i < selectedStations.size(); i++) {
+    totalWeight += stations[selectedStations[i]].weight;
+  }
+
+  // display total weight
+  cout << "Total weight: " << totalWeight << "/" << maxWeight << endl;
+
+  // display maximum profit
+  cout << "Maximum Profit: " << dpTable[numOfItems][maxWeight] << endl;
+
+  return dpTable[numOfItems][maxWeight];
 }
