4181aa9e610d2eac33db38e2deaa72c62144a821
viv <vivian.wgting11@gmail.com>
2024-01-27 19:08:33 +0800

    update kruskal

--- kruskal.cpp
+++ kruskal.cpp
@@ -9,14 +9,13 @@ bool routeComparator(const Route &a, const Route &b) {
     return a.weight < b.weight;
 }
 
-bool mstComparator(const Route &a, const Route &b) {
-    return a.from < b.from;
-}
+char findRoot(char parent[], char vertex) {
+    // if parent of current vertex = vertex itself, it is the root
+    if (parent[vertex - 'A'] == vertex)
+        return vertex;
 
-char findStation(char station[], char route) {
-    if (station[route - 'A'] == route)
-        return route;
-    return findStation(station, station[route - 'A']);
+    // else, find root
+    return findRoot(parent, parent[vertex - 'A']);
 }
 
 // minimum spanning tree
@@ -24,41 +23,40 @@ void kruskal(const vector<Station> &stations)
 {
     // make all routes into one vector
     vector<Route> allRoutes;
-    for(const auto &station : stations) {
+    for(const auto &station : stations)
         allRoutes.insert(allRoutes.end(), station.routes.begin(), station.routes.end());
-    }
-        
-    // sort all routes in non-decreasing order of weight 
-    sort(allRoutes.begin(), allRoutes.end(), routeComparator);
-
     
-    char station[20];
+    // sort all routes from smallest in order of weight 
+    sort(allRoutes.begin(), allRoutes.end(), routeComparator);
+    
+    // initialise station
+    char parent[20];
     for (char c = 'A'; c <= 'T'; ++c)
-    {
-        station[c - 'A'] = c;
-    }
+        parent[c - 'A'] = c;
 
     vector<Route> mst;
     int totalWeight = 0;
 
+    // iterate until stations.size()-1 routes
     for(const auto &route: allRoutes) {
-        char fromStation = findStation(station, route.from);
-        char toStation = findStation(station, route.to);
+        char fromStation = findRoot(parent, route.from);
+        char toStation = findRoot(parent, route.to);
 
+        // check if adding this route will form a cycle
+        // if not, include the edge
         if(fromStation != toStation) {
             mst.push_back(route);
-            station[fromStation - 'A'] = toStation;
+            parent[fromStation - 'A'] = toStation;
             totalWeight += route.weight;
         }
     }
 
-    sort(mst.begin(), mst.end(), mstComparator);
-
-    cout << "Minimum Spanning Tree:" << endl;
+    // print 
+    cout << "Minimum spanning tree:" << endl;
     for (const auto &route : mst)
     {
-        cout << route.from << " - " << route.to << ", Weight: " << route.weight << endl;
+        cout << route.from << " - " << route.to << ", weight: " << route.weight << ", distance: " << route.distance << endl;
     }
 
-    cout << "Total Weight of MST: " << totalWeight << endl;
+    cout << "\nTotal weight of MST: " << totalWeight << endl;
 }
